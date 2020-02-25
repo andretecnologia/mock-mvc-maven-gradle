171d92a726ea981e773958b26573dbcaa1e775cb
enegociosandre <enegocios.andre@gmail.com>
2020-02-25 15:31:41 -0300

    bootstrapping the application

---
+++ src/main/java/br/com/andretecnologia/MockMVCMavenGradle.java
@@ -0,0 +1,13 @@
+package br.com.andretecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMVCMavenGradle {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( MockMVCMavenGradle.class, args );
+    }
+
+}
---
+++ src/test/java/br/com/andretecnologia/MockMVCMavenGradleTests.java
@@ -0,0 +1,13 @@
+package br.com.andretecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockMVCMavenGradleTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
